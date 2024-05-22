e702feb6d872c8f21f1ec8f393012f60776e7fc8
danielhdezr <159938910+danielhdezr@users.noreply.github.com>
2024-05-22 00:09:10 -0700

    Login funcional

---
+++ src/src/Controllers/AuthController.java
@@ -0,0 +1,24 @@
+package Controllers;
+
+import Views.AuthView;
+
+public class AuthController {
+	
+	public AuthView vista;
+
+	public AuthController() {
+		// TODO Auto-generated constructor stub
+		vista = new AuthView();
+	}
+	
+	public void login() {
+    	
+    	vista.loginPanel();
+    }
+	
+	public void registro() {
+		
+		vista.registroPanel();;
+	}
+
+}
---
+++ src/src/Main/Main.java
@@ -0,0 +1,15 @@
+package Main;
+
+import Controllers.AuthController;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AuthController prueba = new AuthController();
+		
+		prueba.login();
+	}
+
+}
